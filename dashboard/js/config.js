d2ef0cda42cd2369c47f09dd80e193f453163300
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-03-28 21:49:49 +0100

    Added the segmentation

--- dashboard/js/config.js
+++ dashboard/js/config.js
@@ -5,7 +5,7 @@ function config() {
             tiles: 'https://raw.githubusercontent.com/acycliq/VS6_MsBrain_A3_maptiles/master/maptiles/262144px/z3/{z}/{y}/{x}.jpg',
             cellData: 'https://api.github.com/repos/acycliq/VS6_MsBrain_A3_datastore/contents/data/z3/cellData?ref=main',
             geneData: 'https://api.github.com/repos/acycliq/VS6_MsBrain_A3_datastore/contents/data/z3/geneData?ref=main',
-            cellCoords: 'https://api.github.com/repos/acycliq/VS6_MsBrain_A3_datastore/contents/data/z3/cellCoords?ref=main',
+            cellBoundaries: 'https://api.github.com/repos/acycliq/VS6_MsBrain_A3_datastore/contents/data/z3/cellBoundaries?ref=main',
             class_name_separator: '.' //The delimiter in the class name string, eg if name is Astro.1, then use the dot as a separator, if Astro1 then use an empty string. It is used in a menu/control to show the class names nested under its broader name
         }
 }
