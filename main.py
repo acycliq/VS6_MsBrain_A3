19209891fbb8265217c5e43d6ae2aec03a9abffe
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2021-06-13 11:11:40 +0100

    Fat typo that messed up the transformation

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def read_spots(cfg):
     data_z3 = data[data.global_z == 3]
 
     logger.info('Data size: %d, %d' % (data_z3.shape[0], data_z3.shape[1]))
-    data_z3 = data_z3.assign(global_x_px = tx(data_z3.global_x.values))
-    data_z3 = data_z3.assign(global_y_px= tx(data_z3.global_y.values))
+    data_z3 = data_z3.assign(global_x_px=tx(data_z3.global_x.values))
+    data_z3 = data_z3.assign(global_y_px=ty(data_z3.global_y.values))
     data_z3 = data_z3.sort_values(by=['global_x_px', 'global_y_px'])
     data_z3 = data_z3.drop(['Unnamed: 0'], axis=1)
     return data_z3
@@ -45,10 +45,14 @@ def label_spots(label_image, spots: np.array) -> np.array:
 
 
 def run(cfg):
-    boundaries = cell_boundaries()
-    # Uncomment the following two lines if you want to get the boundaries from a precached csv file
-    # boundaries = pd.read_csv('cell_boundaries_2.csv')
-    # boundaries['cell_boundaries'] = boundaries.cell_boundaries.apply(json.loads)
+    CACHED_BOUNDARIES = True
+
+    if CACHED_BOUNDARIES:
+        boundaries = pd.read_csv('cell_boundaries_2.csv')
+        boundaries['cell_boundaries'] = boundaries.cell_boundaries.apply(json.loads)
+    else:
+        boundaries = cell_boundaries()
+
     label_image, cell_props = get_label_image(boundaries, cfg)
     spots_df = read_spots(cfg)
 
@@ -65,3 +69,19 @@ if __name__=="__main__":
     cfg = config.DEFAULT
     run(cfg)
     logger.info('Done!')
+
+
+
+
+#
+# barcode_id = data.barcode_id.values
+# nG = len(np.unique(barcode_id))
+# cell_label = data.cell_label.values
+# nC = len(np.unique(cell_label))
+# group_idx = np.array([cell_label, barcode_id])
+# df = pd.DataFrame(npg.aggregate(group_idx, barcode_id, size=[355710, 315], func='count'))
+# df.head()
+#
+# # Get rid of the background
+# df = df.iloc[1:, :]
+# df
